1350a38fec2c55fd15431e13e50c8758f7ceee84
peniel18 <penieletornam18@gmail.com>
2024-10-14 12:14:05 +0000

    fixed artifacts errors

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,20 +4,22 @@ from dataclasses import dataclass
 from pathlib import Path
 import pandas as pd 
 #import numpy as np 
-#from src.logger.Logging import logging
+from src.logger.Logging import logging
 from src.exception.exception import CustomException
+
 from sklearn.model_selection import train_test_split
 
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_source: str = Path("./experiment/train.csv")
-    test_data_source: str = Path("./experiments/test.csv")
-    raw_data_path: str = os.path.join("artifacts", "raw.csv")
-    train_data_path: str = os.path.join("artifacts", "train.csv")
-    test_data_path: str = os.path.join("artifacts", "test.csv")
+    current_dir = os.getcwd() 
+    artifacts_dir = os.path.join(current_dir, "..", ".." ,"artifacts")
 
+    raw_data_path: str = os.path.join(artifacts_dir, "raw.csv")
+    train_data_path: str = os.path.join(artifacts_dir, "train.csv")
+    test_data_path: str = os.path.join(artifacts_dir, "test.csv")
+    dataSource: str = "https://raw.githubusercontent.com/peniel18/Artifacts/refs/heads/main/GemstoneTrain.csv"    
 
 class DataIngestion:
 
@@ -27,28 +29,26 @@ class DataIngestion:
 
 
     def InitialDataIngestion(self):
-        #logging.info("Data Ingestion Started")
+        logging.info("Data Ingestion Started")
         try: 
             # read data from source eg. from API or github
-            data = pd.read_csv("https://raw.githubusercontent.com/sunnysavita10/fsdsmendtoend/main/notebooks/data/gemstone.csv")
-           # logging.info("Reading data")
-
+            data = pd.read_csv(self.ingestion_config.dataSource)
             # make a dir from the directory of raw.csv
             os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)
             # save data 
             data.to_csv(self.ingestion_config.raw_data_path, index=False)
-           # logging.info("Data saved in artifacts folder")
+            logging.info("Data saved in artifacts folder")
 
             # Split data 
             train_data, test_data  = train_test_split(data, test_size=0.25)
-           # logging.info("Train test split Completed")
+            logging.info("Train test split Completed")
 
             train_data.to_csv(self.ingestion_config.train_data_path, index=False)
             test_data.to_csv(self.ingestion_config.test_data_path, index=False)
 
         except Exception as e: 
 
-            raise CustomException(e, sys)
+           raise CustomException(e, sys)
         
         return (
                 self.ingestion_config.train_data_path, 
