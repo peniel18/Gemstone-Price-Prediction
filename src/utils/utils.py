73ec92b0532c8c14400fab466b440d8c9f5ed05f
peniel18 <penieletornam18@gmail.com>
2024-10-15 13:55:43 +0000

    fixed utils functions

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,14 +8,14 @@ from src.exception.exception import CustomException
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 
-def save_object(file_path, obj):
+def save_object(file_path: str, obj):
     try: 
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj: 
             pickle.dump(obj, file_obj)
-
+    
     except Exception as e: 
         raise CustomException(e, sys)
     
@@ -29,7 +29,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             # predictions 
             y_hat = model.predict(X_test)
             test_model_score = r2_score(y_true=y_test, y_pred=y_hat)
-            report[list(model.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
         return report 
     except Exception as e: 
         logging.info("Exception Occured during model training")
