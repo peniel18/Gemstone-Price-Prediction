362d7005955d88709e136667323e32e7438c4e44
peniel18 <penieletornam18@gmail.com>
2024-10-18 20:16:08 +0000

    fixed flask server issues

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template, jsonify 
+from flask import Flask, render_template, jsonify , request
 from src.pipeline.prediction_pipeline import PredictionPipeline, CustomData
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def homepage():
-    render_template("index.html")
+    return render_template("index.html")
 
+
+@app.route('/predict_datapoint', methods = ["GET", "POST"])
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template("form.html")
+    else: 
+        data = CustomData(
+            carat = float(request.form.get("carat")), 
+            depth = float(request.form.get("depth")),
+            table = float(request.form.get("table")), 
+            x = float(request.form.get("X")),
+            y = float(request.form.get("Y")),
+            z = float(request.form.get("Z")),
+            color = request.form.get("color"),
+            clarity = request.form.get("clarity"),     
+        )
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=True)
 
-    
